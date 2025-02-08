3f94013d22ca6311a7d928a72e3235754b931a70
Jacek Pasierb <jack_byk@o2.pl>
2025-02-08 20:51:08 +0100

    Fix - section products correct

--- js/products.js
+++ js/products.js
@@ -2,12 +2,14 @@ const productContainer = document.getElementById("products");
 const productCountSelect = document.getElementById("product-count");
 const popupClose = document.getElementById("popupClose");
 const popupOverlay = document.getElementById("popupOverlay");
+const productsSection = document.querySelector(".products");
+
 let currentCount = 20;
 let currentPage = 1;
 const apiUrl = "https://brandstestowy.smallhost.pl/api/random";
 let isLoading = false;
 
-async function fetchProducts(pageNumber, pageSize) {
+const fetchProducts = async (pageNumber, pageSize) => {
   try {
     const response = await fetch(
       `${apiUrl}?pageNumber=${pageNumber}&pageSize=${pageSize}`
@@ -17,22 +19,23 @@ async function fetchProducts(pageNumber, pageSize) {
       return [];
     }
     const data = await response.json();
+
     return Array.isArray(data) ? data : data.data || [];
   } catch (error) {
     console.error("Error fetching products:", error);
     return [];
   }
-}
+};
 
-function createProduct(product) {
+const createProduct = (product) => {
   const productElement = document.createElement("div");
   productElement.classList.add("product");
   productElement.textContent = ` ID: ${product.id}` || `Produkt`;
   productElement.addEventListener("click", () => openPopup(product));
   return productElement;
-}
+};
 
-async function loadProducts(pageNumber, pageSize) {
+const loadProducts = async (pageNumber, pageSize) => {
   if (isLoading) return;
   isLoading = true;
   const products = await fetchProducts(pageNumber, pageSize);
@@ -44,7 +47,7 @@ async function loadProducts(pageNumber, pageSize) {
     productContainer.appendChild(createProduct(product));
   });
   isLoading = false;
-}
+};
 
 productCountSelect.addEventListener("change", () => {
   currentCount = parseInt(productCountSelect.value, 10);
@@ -53,26 +56,40 @@ productCountSelect.addEventListener("change", () => {
   loadProducts(currentPage, currentCount);
 });
 
-function openPopup(product) {
+const openPopup = (product) => {
   popupContent.innerHTML = `<h2>ID: ${product.id}</h2><p>Nazwa: ${
     Object.keys(product)[1]
   }</p><p>Wartość: ${product.text}</p>`;
   popup.style.display = "block";
   popupOverlay.style.display = "block";
-}
+};
 
-function closePopup() {
+const closePopup = () => {
   popup.style.display = "none";
   popupOverlay.style.display = "none";
-}
+};
 
 popupClose.addEventListener("click", closePopup);
 popupOverlay.addEventListener("click", closePopup);
+
+const productObserver = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && !isLoading) {
+        productObserver.unobserve(productsSection);
+        loadProducts(currentPage, currentCount);
+      }
+    });
+  },
+  {threshold: 0.5}
+);
+
+productObserver.observe(productsSection);
+
 window.addEventListener("scroll", () => {
+  if (isLoading) return;
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 10) {
     currentPage++;
     loadProducts(currentPage, currentCount);
   }
 });
-
-loadProducts(currentPage, currentCount);
