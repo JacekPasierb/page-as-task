4f5a86e99c5ce11fe0399d01e5cc60d20e4ececd
Jacek Pasierb <jack_byk@o2.pl>
2025-02-20 19:08:21 +0100

    Fix/MenuHamnburger - zablokowano mozliwosc scrollowania przy otwartym menu

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,20 @@
 const modal = document.getElementById("myModal");
 const modalButton = document.getElementById("toggleModalButton");
+const body = document.body;
 
 modalButton.addEventListener("click", function () {
   if (modal.style.display === "block") {
     modal.style.display = "none";
+    body.classList.toggle("no-scroll");
   } else {
     modal.style.display = "block";
+    body.classList.toggle("no-scroll");
   }
 });
 
 modal.addEventListener("click", () => {
   if ((modal.style.display = "block")) {
     modal.style.display = "none";
+    body.classList.remove("no-scroll");
   }
 });
